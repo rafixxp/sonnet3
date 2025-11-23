a882a79019f7a47eac998b32a3df098b60891d24
rafixxp <xnuxerdev@gmail.com>
2025-11-23 22:17:21 +0700

    fix:changing a model

--- src/service/index.js
+++ src/service/index.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://openrouter.ai/api/v1',
+  baseURL: 'https://api.groq.com/openai/v1',
   headers: {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + import.meta.env.VITE_OPENROUTER_API_KEY,
-    'HTTP-Referer': import.meta.env.VITE_APP_URL,
     'X-Title': 'Selidik'
   }
 })
@@ -13,10 +12,13 @@ const api = axios.create({
 const send = async (message) => {
     const messages = `${message}. Berikan respons yang atraktif dan menarik seperti manusia dalam format HTML 5, tetapi hanya gunakan tag <h5>, <span>, <p>, <i>, <strong> untuk cetak teks saja`
     const res = await api.post('/chat/completions', {
-        model: 'x-ai/grok-4.1-fast',
+        model: "openai/gpt-oss-120b",
+        max_completion_tokens: 1000,
         messages: [
-        { role: 'user', content: messages }
-        ]
+          { role: 'user', 
+            content: messages 
+          }
+        ],
     })
 
     return res.data.choices[0].message.content
